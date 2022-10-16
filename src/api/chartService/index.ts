55250dba2179647f05b1d9808360ef4da8e1a8cb
Александр <alex@MacBook-Pro-Aleksandr.local>
2022-10-17 02:27:53 +0300

    refactor: remove logs & add types

--- src/api/chartService/index.ts
+++ src/api/chartService/index.ts
@@ -4,12 +4,10 @@ import { ILineChartResponse, IPieChartResponse } from '@/types/api/chart';
 
 export const fetchLineChartData = async ():Promise<Array<ILineChartResponse>> => {
   const { data } = await HTTPClient.$get(MOCK_DATA_URL.LINE);
-  console.log(data);
   return data;
 };
 
 export const fetchPieChartData = async ():Promise<Array<IPieChartResponse>> => {
   const { data } = await HTTPClient.$get(MOCK_DATA_URL.PIE);
-  console.log(data);
   return data;
 };
